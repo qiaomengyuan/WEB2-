bc407bbc0add06caaa39df9047b42c7695ec3a8c
qiao <qiaomengyuan@foxmail.com>
2023-09-07 17:10:02 +0800

    hotfix commit2

--- src/main/java/com/qiao/controller/DeptController.java
+++ src/main/java/com/qiao/controller/DeptController.java
@@ -38,7 +38,7 @@ public class DeptController {
     @Log
     @DeleteMapping("{id}")
     public Result list(@PathVariable Integer id){
-        log.info("根据id删除部门:{}",id);
+        log.info("根据id删除部门hotfix-test:{}",id);
         log.info("根据id1删除部门乔梦圆:{}",id);
         deptService.delete(id);
         return Result.success();
